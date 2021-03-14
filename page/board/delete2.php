1459c05a40ccdfa7d2125772e862fe62ce0c7e94
signalsecret <62201730+signalsecret@users.noreply.github.com>
2021-03-14 12:35:48 +0900

    first version

---
+++ page/board/delete2.php
@@ -0,0 +1,8 @@
+<?php
+	include $_SERVER['DOCUMENT_ROOT']."/db2.php";
+	
+	$bno = $_GET['idx'];
+	$sql = mq("delete from board where idx='$bno';");
+?>
+<script type="text/javascript">alert("삭제되었습니다.");</script>
+<meta http-equiv="refresh" content="0 url=/list2.php" />
